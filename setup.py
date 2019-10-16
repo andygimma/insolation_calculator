8c4921468d81637405e3cbdf6221424d1bbd6998
andygimma <andrewgimma@protonmail.com>
2019-10-16 19:24:32 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='insolation_calculator',
+      version='0.1',
+      description='calculate insolation based on latitude and day',
+      url='https://github.com/andygimma/insolation_calculator',
+      author='Andy Gimma',
+      author_email="andrewgimma@protonmail.com",
+      packages=['insolation_calculator'],
+      zip_safe=False)
