6326cc1994046602d0f14eabd588f419b773c05e
andygimma <andrewgimma@protonmail.com>
2019-10-16 21:21:03 -0400

    Calculate daily average

--- insolation_calculator/insolation_calculator.py
+++ insolation_calculator/insolation_calculator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from climlab.solar.orbital import OrbitalTable
 from climlab.solar.insolation import daily_insolation
+from functools import reduce
 
 DAYS_IN_YEAR = 365
 YEARS_IN_SIMULATION = 20
@@ -11,8 +12,13 @@ class InsolationCalculator(object):
         self.latitude = latitude
         self.temps = []
 
-    def calculate_days_average(self, day):
+    def _daily_insolation(self, day):
         years = np.linspace(-YEARS_IN_SIMULATION, 0, YEARS_IN_SIMULATION)
         orb = OrbitalTable.interp(kyear=years)
         i = daily_insolation(lat=self.latitude, day=day, orb=orb)
         return i.to_dict()['data']
+
+    def daily_average(self, day):
+        insolation_data = self._daily_insolation(day)
+        acc = reduce((lambda x, y: x + y), insolation_data)
+        return acc / len(insolation_data)
--- insolation_calculator/insolation_calculator_test.py
+++ insolation_calculator/insolation_calculator_test.py
@@ -11,15 +11,23 @@ class InsolationCalculatorTest(unittest.TestCase):
         calc = InsolationCalculator(latitude=expected_latitude)
         self.assertEqual(calc.latitude, expected_latitude)
 
-    def test_calculate_days_average(self):
+    def test_daily_insolation(self):
         # NOTE These assertions are very specific to the climlab library
         # and can be considered integration tests.
         expected_latitude = 0.1
         day = 1
         calc = InsolationCalculator(latitude=expected_latitude)
-        returned_value = calc.calculate_days_average(day)
+        returned_value = calc._daily_insolation(day=day)
         self.assertTrue(type(returned_value) == list)
         self.assertTrue(len(returned_value) > 0)
+
+    def test_daily_average(self):
+        expected_latitude = 0.1
+        day = 1
+        calc = InsolationCalculator(latitude=expected_latitude)
+        returned_value = calc.daily_average(day=day)
+        self.assertTrue(type(returned_value) == float)
+        self.assertTrue(returned_value > 0)
 
 
 if __name__ == '__main__':
