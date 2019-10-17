2d567c2dd07c3e4f86359040108f059a5789eb1c
andygimma <andrewgimma@protonmail.com>
2019-10-16 20:55:00 -0400

    Set up initial function and test, using climlab. Create requirements.txt

--- insolation_calculator/insolation_calculator.py
+++ insolation_calculator/insolation_calculator.py
@@ -1,3 +1,17 @@
+import numpy as np
+from climlab.solar.orbital import OrbitalTable
+from climlab.solar.insolation import daily_insolation
+
+DAYS_IN_YEAR = 365
+YEARS_IN_SIMULATION = 20
+
+
 class InsolationCalculator(object):
     def __init__(self, latitude):
         self.latitude = latitude
+        self.temps = []
+
+    def calculate_days_average(self, day):
+        years = np.linspace(-20, 0, 20)
+        orb = OrbitalTable.interp(kyear=years)
+        return daily_insolation(lat=self.latitude, day=day, orb=orb)
--- insolation_calculator/insolation_calculator_test.py
+++ insolation_calculator/insolation_calculator_test.py
@@ -1,14 +1,24 @@
 import unittest
-import insolation_calculator
+import xarray
+
+from insolation_calculator import InsolationCalculator
 
 
 class InsolationCalculatorTest(unittest.TestCase):
 
     def test_accepts_args(self):
         expected_latitude = 0.1
-        calc = insolation_calculator.InsolationCalculator(
-            latitude=expected_latitude)
+        calc = InsolationCalculator(latitude=expected_latitude)
         self.assertEqual(calc.latitude, expected_latitude)
+
+    def test_calculate_days_average(self):
+        expected_latitude = 0.1
+        day = 1
+        calc = InsolationCalculator(latitude=expected_latitude)
+        returned_value = calc.calculate_days_average(day)
+        self.assertTrue(isinstance(
+            returned_value, xarray.core.dataarray.DataArray))
+        self.assertTrue(len(returned_value.to_dict()['data']) > 0)
 
 
 if __name__ == '__main__':
